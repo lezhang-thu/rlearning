79af9f5ae9dd5579214ce731febeb008ac878538
Le Zhang <lezhang.thu@gmail.com>
2018-10-16 23:14:19 +0800

    Update mcts_improve.py

--- mcts_improve.py
+++ mcts_improve.py
@@ -4,41 +4,45 @@ class MCTS:
     def __init__(
         self,
         original_image,
-        global_feature,
-        crop_img,
         sess_policy,
         sess_evaluate,
-        tau,
         c_puct,
         num_virtual_threads=0,
         virtual_loss=0
     ):
+        original_image = np.expand_dims(original_image, axis=0)
         self.original_image = original_image
-        self.global_feature = global_feature
+        with sess_policy.as_default():
+            self.global_feature = sess_policy.run(
+                global_feature_placeholder,
+                feed_dict={
+                    image_placeholder_policy: original_image
+                }
+            )
 
         self.root = Node(
-            crop_img,
+            crop_img=original_image,
             hidden_state=np.zeros([1, 1024]),
             cell_state=np.zeros([1, 1024]),
             ratio=np.repeat([[0, 0, 20, 20]], 1, axis=0),
-            terminal=False
+            terminal=np.zeros(1, dtype=bool)
         )
-        self.sess = {'policy': sess_policy, 'evaluate': sess_evaluate}
+
         self.param = {
-            'tau': tau,
             'c_puct': c_puct,
             'num_virtual_threads': num_virtual_threads,
             'virtual_loss': virtual_loss
         }
-        with self.sess['evaluate'].as_default():
+        with sess_evaluate.as_default():
             self.baseline = self.sess['evaluate'].run(
                 score_func,
                 feed_dict={
-                    image_placeholder_evaluate: original_img
+                    image_placeholder_evaluate: original_image
                 }
             )[0]
+        self.sess = {'policy': sess_policy, 'evaluate': sess_evaluate}
 
-    def _get_evaluation(self, crop_image):
+    def _get_evaluation(self, crop_img):
         with self.sess['evaluate'].as_default():
             return self.sess['evaluate'].run(
                 score_func, feed_dict={
@@ -46,7 +50,7 @@ class MCTS:
                 }
             )[0] - self.baseline
 
-    def _get_action_prob(self, crop_image, hidden_state, cell_state):
+    def _get_action_prob(self, crop_img, hidden_state, cell_state):
         with self.sess['policy'].as_default():
             return self.sess['policy'].run(
                 (action, h, c),
@@ -68,7 +72,7 @@ class MCTS:
         for leaf in visit_times:
             value_ls.append(self._rollout(leaf))
             self._expand(leaf)
-            if leaf.state['terminal'] is False:
+            if not leaf.state['terminal'][0]:
                 leaf.state = None  #leaf is updated to an internal node, if it is not of terminal state
         for i, (leaf, freq) in enumerate(visit_times.items()):
             self._backup(leaf, freq, value_ls[i])
@@ -93,79 +97,71 @@ class MCTS:
                     (1 + c.parent_info['statistics']['Nsa'])
                 ):
                     max_child = c
-            #'the rollout statistics are updated as if it has lost n_vl games'
-            if s.parent_info is not None:
-                s.parent_info['statistics']['Nsa'] += self.param['virtual_loss']
-                s.parent_info['statistics']['Wsa'] -= self.param['virtual_loss']
-                s.parent_info['statistics']['Qsa'] = s.parent_info[
-                    'statistics'
-                ]['Wsa'] / s.parent_info['statistics']['Nsa']
             s = max_child
+            #'the rollout statistics are updated as if it has lost n_vl games'
+            s.parent_info['statistics']['Nsa'] += self.param['virtual_loss']
+            s.parent_info['statistics']['Wsa'] -= self.param['virtual_loss']
+            s.parent_info['statistics']['Qsa'] = s.parent_info['statistics'][
+                'Wsa'
+            ] / s.parent_info['statistics']['Nsa']
         return s
 
     def _rollout(self, leaf):
+        #notice the np.copy
         crop_img, hidden_state, cell_state, ratio, terminal = (
             leaf.state['crop_img'], leaf.state['hidden_state'],
-            leaf.state['cell_state'], leaf.state['ratio'],
-            leaf.state['terminal']
+            leaf.state['cell_state'], np.copy(leaf.state['ratio']),
+            np.copy(leaf.state['terminal'])
         )
-        while terminal is False:
+
+        while not terminal[0]:
             #sample an action
             action_prob, hidden_state, cell_state = self._get_action_prob(
                 crop_img, hidden_state, cell_state
             )
-            action_prob = np.reshape(action_prob, num_actions)
 
-            action_ls = np.random.choice(num_actions, 1, action_prob)
-            ratio_ls = np.repeat(ratio, 1, axis=0)
-            terminal_ls = np.zeros(1, dtype=bool)
-            ratio_ls, terminal_ls = command2action(
-                action_ls, ratio_ls, terminal_ls
+            action_sample = np.random.choice(
+                num_actions, 1, p=np.reshape(action_prob, (-1))
             )
-            bbox = generate_bbox(self.original_image, ratio_ls)
-
-            crop_img_ls = [
-                crop_input(self.original_image, bbox)
-                if terminals_ls[0] is False else crop_img
-            ]
-
-            crop_img = crop_img_ls[0]
-            ratio = ratio_ls[0]
-            terminal = terminal_ls[0]
+            ratio, terminal = command2action(action_sample, ratio, terminal)
+            if not terminal[0]:
+                bbox = generate_bbox(self.original_image, ratio)
+                crop_img = crop_input(self.original_image, bbox)
 
         #reach a terminal state, then compute ranking scores
         return self._get_evaluation(crop_img)
 
     def _expand(self, leaf):
-        if leaf.state['terminal'] is True:
+        if leaf.state['terminal'][0]:
             return
         action_prob, hidden_state, cell_state = self._get_action_prob(
             leaf.state['crop_img'], leaf.state['hidden_state'],
             leaf.state['cell_state']
         )
+        action_prob = np.reshape(action_prob, (-1))
 
-        action_ls = np.array(range(num_actions))
+        action_ls = np.array(range(num_actions), dtype=np.int32)
         ratio_ls = np.repeat(
             leaf.state['ratio'], num_actions, axis=0
         )  #intial ratio is of batch_size 1
-        terminal_ls = np.zeros(num_actions, dtype=bool)
+        terminal_ls = np.repeat(
+            leaf.state['terminal'], num_actions, axis=0
+        )  #intial terminal is of batch_size 1
         ratio_ls, terminal_ls = command2action(action_ls, ratio_ls, terminal_ls)
-        bbox = generate_bbox(self.original_image, ratio_ls)
+        bbox_ls = generate_bbox(
+            np.repeat(self.original_image, num_action, axis=0), ratio_ls
+        )
+        crop_img_ls = crop_input(self.original_image, bbox_ls)
 
-        crop_img_ls = [
-            crop_input(self.original_image, bbox)
-            if terminal_ls[i] is False else leaf.state['crop_img']
-            for i in range(num_actions)
-        ]
-        s.children = []
+        leaf.children = []
         for i in range(num_actions):
             s.children.append(
                 Node(
-                    crop_img_ls[i],
+                    crop_img=np.expand_dims(crop_img_ls[i], axis=0),
                     hidden_state=hidden_state,
                     cell_state=cell_state,
-                    ratio=ratio_ls[i],
-                    terminal=terminal_ls[i],
+                    ratio=np.expand_dims(ratio_ls[i], axis=0),
+                    terminal=np.expand_dims(terminal_ls[i], axis=0),
                     parent=s,
                     Nsa=0,
                     Wsa=0,
@@ -192,7 +188,8 @@ class MCTS:
         return generate_bbox(self.original_image, self.root.state['ratio'])
 
     def reach_terminal(self):
-        if self.root.state is not None and self.root.state['terminal'] is True:
+        if self.root.state is not None and self.root.state['terminal'][0
+                                                                      ] is True:
             return True
         return False
 
@@ -213,5 +210,3 @@ class MCTS:
         self.root = max_child
         root.parent_info = None
         return action
-        
-        
